22152896f2c9166241a9ddf3449eb6ca74888291
Leonardo Sampaio <leonardosampaio@mpma.mp.br>
2021-06-23 22:13:26 -0300

    preview

--- sh/merge.sh
+++ sh/merge.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=merged.pdf mine1.pdf mine2.pdf
+cd ../preview
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=../preview.pdf 1.pdf 2.pdf 3.pdf 4.pdf 5.pdf 6.pdf  7.pdf\
+ 8.pdf 9.pdf 10.pdf 11.pdf 12.pdf 13.pdf 14.pdf 15.pdf 16.pdf 17.pdf 18.pdf 19.pdf 20.pdf
